d5ac04d6899d5b9b026a60b7100412d7b5df373a
Ryan Alyn Porter <rap@endymion.com>
2013-11-14 14:59:00 -0500

    Got "rake spec" working.
    It expects the specs to be organized into subfolders.  Okay sure, we'll use subfolders for that one spec file...

--- spec/models/record-locator_spec.rb
+++ spec/models/record-locator_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
-require File.expand_path(File.dirname(__FILE__) + '/../lib/record-locator-util')
-require File.expand_path(File.dirname(__FILE__) + '/../models/book')
+require File.expand_path(File.dirname(__FILE__) + '../../../lib/record-locator-util')
+require File.expand_path(File.dirname(__FILE__) + '../../../models/book')
 
 describe 'RecordLocator' do
 
